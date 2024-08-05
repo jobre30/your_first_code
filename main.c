59097f9cfd52fb569a9caba05346f991c2ffd297
jobre30 <123345780+jobre30@users.noreply.github.com>
2024-08-05 16:51:20 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,19 @@ int main()
   }
   return 0;
 }
+
+int main ()
+{
+ int i;
+for (i = 0; i < 32;  i++)
+        {
+        printf(''Hellow, School\n'');
+ }
+   return 0;
+}
+int main ()
+{
+        
+
+       
+                
